aeaf24c5d5075605741179e7e8ff349a6e62a0ac
Cklla <github@cklla.ch>
2026-05-01 14:04:34 +0200

    fix(gradle): remplacement moshi-codegen par l'approche réflexion pour compatibilité Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
-  id("kotlin-kapt")
 }
 
 android {
@@ -43,6 +42,7 @@ dependencies {
   implementation("com.google.android.material:material:1.8.0")
   implementation("androidx.annotation:annotation:1.6.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("androidx.activity:activity-ktx:1.9.0")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -56,7 +56,6 @@ dependencies {
   // Moshi : pour lire/écrire le JSON
   implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
   implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
-  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
   // Coroutines : pour l'asynchrone
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
